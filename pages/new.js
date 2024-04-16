ec346627aa354ce3c9eacffb6917541c41748ec4
Alex Berka <alexanderberka@gmail.com>
2024-04-15 20:15:56 -0500

    Initialized MVP files, firebase connection, api calls

---
+++ pages/new.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MemberForm from '../components/forms/MemberForm';
+
+export default function NewMember() {
+  return (
+    <div>
+      <MemberForm />
+    </div>
+  );
+}
